989a6b17b34413df401e95cf742641cb5db7c422
wudi <wuchendi96@gmail.com>
2025-04-25 11:16:43 +0800

    feat: 添加计算连乘积和数组处理功能，支持找出相同值和差异值

--- other/0-1.js
+++ other/0-1.js
@@ -1,3 +1,8 @@
+// 1. 定义一个计算连乘积的函数，传入一个正整数，返回连乘数结果。连乘数指从小到大能够整除这个数的数字的乘积。
+
+//    fn(10, 2)的计算过程为 10*8*6*4*2*1 = 3840
+//    fn(100, 20)的计算过程为 100*80*60*40*20*1 = 3840000000
+
 function fn(start, step) {
   if (start < 1 || step < 1 || !Number.isInteger(start) || !Number.isInteger(step)) {
     throw new Error("输入必须为正整数");
